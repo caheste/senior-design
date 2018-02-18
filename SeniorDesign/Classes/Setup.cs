3b2006d6889b2c248ca140de1a0e7300c4b315c8
caheste <caheste@g.clemson.edu>
2018-02-18 15:58:37 -0500

    Add files via upload

---
+++ SeniorDesign/Classes/Setup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InTheHand;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Ports;
+using InTheHand.Net.Sockets;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SeniorDesign.Classes
+{
+    public class Setup
+    {
+
+        public static bool Connect()
+        {
+            BluetoothClient client = new BluetoothClient();
+            MessageBox.Show("Attempting to connect as client.");
+            return false;
+        }
+
+
+
+    }
+}
